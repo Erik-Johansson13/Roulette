774c6770ca4fd8ba18d585a29d651020156be65b
Erik-Johansson13 <13ej1018@amal.se>
2024-09-17 10:30:54 +0200

    Add project files.

---
+++ Roulette/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Roulette
+// A Roulette game in which the user can bet certain amount of money
+// on either red or black, odd or even, a specific number or green
+// The user should be able to deposit or withdraw their money in a bank outsid of the game
+// If the user goes bankrupt or makes enough money they will be able to exit
+// 
